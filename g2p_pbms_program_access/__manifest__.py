12fdac6ccf72c842b9cce5dd940809ef1c40f630
Venky Ganapathy <159788002+venky-ganapathy@users.noreply.github.com>
2026-04-17 10:48:34 +0530

    Bump version from 3.0.1 to 3.1.0
    Preparing for 3.1.0 Tag

--- g2p_pbms_program_access/__manifest__.py
+++ g2p_pbms_program_access/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "PBMS Program Access",
-    "version": "3.0.1",
+    "version": "3.1.0",
     "summary": "OpenG2P PBMS Program Access",
     "description": "OpenG2P PBMS Program Access",
     "category": "G2P",
